1694a76eacea465bb55c69409145e139d6370789
kys97 <lolla44@naver.com>
2021-11-10 20:28:13 +0900

    asdf

---
+++ ViewPager/app/src/main/java/com/example/viewpager/CheckRoleActivity.kt
@@ -0,0 +1,29 @@
+package com.example.viewpager
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.example.viewpager.databinding.ActivityCheckRoleBinding
+import com.google.android.material.tabs.TabLayoutMediator
+
+class CheckRoleActivity : AppCompatActivity() {
+    val binding by lazy { ActivityCheckRoleBinding.inflate(layoutInflater) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        val fragmentList = listOf(FragmentA(), Info(), FragmentC())
+        val adapter = FragmentAdapter(this)
+        adapter.fragmentList = fragmentList
+        binding.viewPager.adapter = adapter
+
+        val tabTitles = listOf<String>("지도", "홈", "기록")
+        TabLayoutMediator(binding.tabLayout, binding.viewPager) { tab, position ->
+            tab.text = tabTitles[position]
+        }.attach()
+
+        val message = intent.getStringExtra("userType")
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+    }
+}
---
+++ ViewPager/app/src/main/java/com/example/viewpager/Loading.java
@@ -0,0 +1,39 @@
+package com.example.viewpager;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.viewpager.login.Login;
+
+public class Loading extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.loading);
+
+        SharedPreferences data = getSharedPreferences("login_data", Activity.MODE_PRIVATE);
+        Boolean login_check = data.getBoolean("LoginCheck",false);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //SharedPreferences 사용
+
+                Intent intent;
+                if(login_check)
+                    intent = new Intent(getApplicationContext(), MainActivity.class);
+                else
+                    intent = new Intent(getApplicationContext(), Login.class);
+
+                startActivity(intent);
+            }
+        }, 2000);
+    }
+}
